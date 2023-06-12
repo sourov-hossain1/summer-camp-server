bef732de0e1ad944d1dd852c3628b1ccacf183a8
sourov-hossain1 <sourovhossain2122006@gmail.com>
2023-06-11 18:00:52 -0700

    user api added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w2ynlfu.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,12 +30,15 @@ async function run() {
         const popularCollection = client.db("summerDB").collection("popular");
         const classesCollection = client.db("summerDB").collection("classes");
         const cartsCollection = client.db("summerDB").collection("carts");
+        const usersCollection = client.db("summerDB").collection("users");
 
+        // popular api
         app.get('/popular', async(req, res) => {
             const result = await popularCollection.find().toArray();
             res.send(result);
         });
 
+        // calsses api
         app.get('/classes', async(req, res) => {
             const result = await classesCollection.find().toArray();
             res.send(result);
@@ -53,12 +56,28 @@ async function run() {
             res.send(result);
         })
 
-        app.post('/carts', async(req, res) => {
-            const item = req.body;
-            console.log(item);
-            const result = await cartsCollection.insertOne(item);
+       app.post('/carts', async(req, res) =>{
+        const items = req.body;
+        const result = await cartsCollection.insertOne(items);
+        res.send(result);
+       })
+
+        // users post api
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
             res.send(result);
-        })
+           })
+
+
+       
+
+    app.delete('/carts/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await cartsCollection.deleteOne(query);
+        res.send(result);
+    })
 
 
         // Send a ping to confirm a successful connection
