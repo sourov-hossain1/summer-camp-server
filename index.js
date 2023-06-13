31ac09c049fd9d9974fb282fecf65e3e26212cc8
sourov-hossain1 <sourovhossain2122006@gmail.com>
2023-06-12 18:16:06 -0700

    verify admin using jwt

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJwt = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message: 'unauthorized access'});
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+            return res.status(401).send({error: true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w2ynlfu.mongodb.net/?retryWrites=true&w=majority`;
@@ -36,7 +52,7 @@ async function run() {
 
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '12h'})
 
             res.send({token})
         })
@@ -54,14 +70,20 @@ async function run() {
         });
 
         // class cart collection
-        app.get('/carts', async (req, res) => {
-            // const email = req.query.email;
+        app.get('/carts', verifyJwt, async (req, res) => {
+            const email = req.query.email;
             // console.log(email)
-            // if(!email){
-            //     res.send([]);
-            // }
-            // const query = {email: email};
-            const result = await cartsCollection.find().toArray();
+            if(!email){
+                res.send([]);
+            }
+
+            const decodedEmail = req.decoded.email;
+            if(email !== decodedEmail){
+                return res.status(403).send({error: true, message: 'forbidden access'})
+            }
+
+            const query = {email: email};
+            const result = await cartsCollection.find(query).toArray();
             res.send(result);
         })
 
